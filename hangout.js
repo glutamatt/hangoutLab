a64ea77f11606fb1a7ae7bbb5d978d317ee3769a
Mathieu Morlon <mathieu.morlon@5emegauche.com>
2012-12-05 17:33:50 +0100

    canvas multiplayer

--- hangout.js
+++ hangout.js
@@ -1,4 +1,6 @@
 var webRoot = 'http://hangout.lab.local/';
 
+webRoot = 'https://raw.github.com/glutamatt/hangoutLab/master/';
+
 document.write("<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'><\/script>");
 document.write("<script src='" + webRoot + "boot.js'><\/script>");
